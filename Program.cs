da70736efd72d63d1e84f8cbeca6eb0a78b594f5
Mazen K. Mrad <Mazen.k.Mrad@kagsi.com>
2021-09-22 08:44:45 +0300

    test

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace QDLottery.App
 {
     public class Program
     {
+        //test
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
